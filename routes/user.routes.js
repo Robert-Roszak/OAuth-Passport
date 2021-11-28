78a6c1fc5043159bf31e79498e45b09a0a294b55
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2021-11-28 12:10:25 +0100

    Add welcome user message

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/logged', (req, res) => {
-  res.render('logged');
+const isLogged = (req, res, next) => {
+  if(!req.user){
+    res.redirect('/user/no-permission');
+  } else {
+    next();
+  }
+};
+
+router.get('/logged', isLogged, (req, res) => {
+  const name = req.user.displayName;
+  const photo = req.user.photos[0].value;
+  res.render('logged', {name: name, photo: photo});
+});
+
+router.get('/profile/settings', isLogged, (req,res) => {
+  res.render('profile/settings');
+});
+
+router.get('/profile', isLogged, (req,res) => {
+  res.render('profile/profile');
 });
 
 router.get('/no-permission', (req, res) => {
